c0251830778eacd43ff6dddd8cef6c7c64f56377
James Cooper <james.ioioio55@gmail.com>
2023-04-09 19:09:12 -0600

    continuing to update app.js file

--- app.js
+++ app.js
@@ -13,13 +13,31 @@ app.set('view engine','ejs');
 app.get("/", function(req,res){
     
     var today = new Date();
+    var currentDay = today.getDay()
+    var weekDate = ""
     var text = "";
-    if (today.getDay() === 6 || today.getDay() === 0){
-        text = "It is currently the weekend.";
-    }else{
-        text = "It is currently a weekday.";
+    if (currentDay === 0 || currentDay === 6){
+        weekDate = "weekend";
+    } else {
+        weekDate = "weekday";
     }
-    res.send(text);
+    
+    if (currentDay === 0 ){
+        text = "Sunday";
+    }else if(currentDay=== 1 ){
+        text = "Monday";
+    }else if(currentDay=== 2 ){
+        text = "Tuesday";
+    }else if(currentDay=== 3 ){
+        text = "Wednesday";
+    }else if(currentDay=== 4 ){
+        text = "Thursday";
+    }else if(currentDay=== 5 ){
+        text = "Friday";
+    }else if(currentDay=== 6 ){
+        text = "Saturday";
+    }
+    res.render('list',{weekDate:weekDate,userDate:text});
 
 });
 
