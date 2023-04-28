4a53a082e3418698b57af25ae8d752525180f581
James Cooper <james.ioioio55@gmail.com>
2023-04-27 21:28:00 -0600

    Updated the EJS list file to use for.each loop, updated app.js to read the DB for list of items, and add additional items

--- app.js
+++ app.js
@@ -11,24 +11,88 @@ app.use(express.static('public'));
 
 app.set('view engine','ejs');
 
-let addList = [];
-let workList = [];
-
 dbName = "toDoListDB"
+
+
+// Mongoose section //
 
 mongoose.connect('mongodb://127.0.0.1:27017/'+ dbName);
 
+const itemsSchema = ({
+    name: String
+});
+
+
+const Item = mongoose.model("Item", itemsSchema);
+
+function createEntry(itemName){
+    const item = new Item({
+        name: itemName,
+    });
+    
+    item.save();
+}
+
+function exitMongoose(){
+    mongoose.connection.close()
+}
+
+// This section is to add the default values into DB
+
+
+const item1 = new Item({
+    name: "Welcome to your todo List",
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item",
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item",
+});
+
+const defaultArray = [item1,item2,item3]
+
+
+
+
+
+// EJS and application section
 
 app.get("/", function(req,res){
+    
 
-    res.render('list',{listTitle:date.getDate(),list:addList});
+    
+    
+    Item.find()
+    .then(items => {
+        if (items.length === 0){
+            Item.insertMany(defaultArray)
+            .then(() => {
+                console.log("Insert Success");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+            res.redirect('/');
+        }else {
+            res.render('list',{listTitle:date.getDate(),list:items});
+        }
+
+        
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    
 
 });
 
-app.get("/work",function(req,res){
+// app.get("/work",function(req,res){
 
-    res.render('list',{listTitle:"Work List",list:workList});
-});
+//     res.render('list',{listTitle:"Work List",list:workList});
+// });
 
 app.get("/about",function(req,res){
 
@@ -36,15 +100,15 @@ app.get("/about",function(req,res){
 });
 
 app.post("/", function(req, res){
-        addList.push(req.body.toDoList);
-        res.redirect("/");
+    createEntry(req.body.toDoList)
+    res.redirect("/");
 
 });
 
-app.post("/work", function(req, res){
-    workList.push(req.body.toDoList);
-    res.redirect("/work");
-});
+// app.post("/work", function(req, res){
+//     workList.push(req.body.toDoList);
+//     res.redirect("/work");
+// });
 
 
 app.listen(3000, function(){
